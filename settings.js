57f5691a8c8f73082296b42c86eeb0754f85c47a
Kavindu170 <ekavindu12@gmail.com>
2025-06-27 02:13:10 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID,
+SESSION_ID: process.env.VAJIRA-MD=fpwhmKJQ#5DhvzgCcmOX99g3-j4oWo1mkdhuTlaTkwYaNc26uRzI === undefined ? '' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '94719199757' : process.env.OWNER_NUMBER,
 N_JID: process.env.N_JID=== undefined ? '‌': process.env.N_JID,    
 PREFIX: process.env.PREFIX || '.' ,
